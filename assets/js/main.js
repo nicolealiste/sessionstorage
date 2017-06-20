69e1df7cb8ef463816158e8ae7fab231c5b122ad
Nicole Aliste <nicole.alistecastillo@gmail.com>
2017-06-20 18:58:24 -0400

    se completa archivo js y se finaliza ejercicio

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,9 +7,14 @@ function saveData(){
 }
 // en la siguiente función un for recorre sessionStorage donde getItem recupera los datos guardados anteriormente
 function recoverData(){
-	for(var i=0; i<sessionStorage.length; i++){
-		var name=sessionStorage.key(i);
-		var clave= sessionStorage.getItem(name);
-		document.getElementById("data").innerHTML = "<div>"+"Nombre: " + name + "<br/> Clave: " + clave + "</div>";
+	for(var i = 0; i < sessionStorage.length; i++){
+		var name = sessionStorage.key(i);
+		var clave = sessionStorage.getItem(name);
+		document.getElementById("data").innerHTML = "<div> Nombre : " + name + "<br/> Clave : " + clave + "</div>";
 	}
 }
+
+function cleanData(){
+	document.getElementById("data").innerHTML = "Limpiada vista. Los datos permanecen";	
+}
+function borrarTodo(){sessionStorage.clear(); recoverData();}
